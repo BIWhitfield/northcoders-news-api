e2f5dc6b2fdaa5a11ddca7d6c0ba191c3d8abf86
BIWhitfield <beniwhitfield@gmail.com>
2017-06-23 17:21:30 +0100

    [apiTests] add tests for get and post requests

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,42 +1,77 @@
-process.env.NODE_ENV = 'test';
-const {expect} = require('chai');
-const request = require('supertest');
-const server = require('../server');
-const saveTestData = require('../seed/test.seed');
-const config = require('../config');
-const db = config.DB[process.env.NODE_ENV] || process.env.DB;
-const mongoose = require('mongoose');
+process.env.NODE_ENV = "test";
+const { expect } = require("chai");
+const request = require("supertest");
+const server = require("../server");
+const saveTestData = require("../seed/test.seed");
+// const config = require("../config");
+// const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+const mongoose = require("mongoose");
 
-describe('API', function () {
-  // let usefulIds;
-  beforeEach((done) => {
-    mongoose.connection.dropDatabase()
-      .then(() => saveTestData(db, done));
+describe("API", function() {
+  let usefulData;
+  beforeEach(done => {
+    mongoose.connection
+      .dropDatabase()
+      .then(saveTestData)
+      .then(data => {
+        usefulData = data;
+        console.log(`Useful data: ${Object.keys(usefulData)}`);
+        done();
+      })
+      .catch(done);
   });
-  describe('GET /', function () {
-    it('responds with status code 200', function (done) {
-      request(server)
-        .get('/')
-        .end((err, res) => {
-          if (err) done(err);
-          else {
-            expect(res.status).to.equal(200);
-            done();
-          }
-        });
-    });
-  });
-  describe('GET /api/topics', () => {
-    it('responds with topics', (done) => {
-      request(server)
-      .get('/api/topics')
-      .end((err, res) => {
-        console.log(res.body)
+  describe("GET /api/topics", () => {
+    it("responds with topics", done => {
+      request(server).get("/api/topics").end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.body.topics).to.be.an('array');
+        expect(res.body.topics).to.be.an("array");
         expect(res.body.topics.length).to.equal(3);
         done();
       });
+    });
+  });
+  describe("GET /api/topics/:topic_id/articles", () => {
+    it("responds with articles in a certain topic", done => {
+      request(server).get("/api/topics/football/articles").end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body.articles[0].belongs_to).to.equal("football");
+        done();
+      });
+    });
+  });
+  describe("GET /api/articles", () => {
+    it("responds with all articles", done => {
+      request(server).get("/api/articles").end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.articles).to.be.an("array");
+        expect(res.body.articles.length).to.equal(2);
+        done();
+      });
+    });
+  });
+  describe("GET /api/articles/:article_id/comments", () => {
+    it("responds with all comments for a particular article", done => {
+      request(server)
+        .get(`/api/articles/${usefulData.articles[0]._id}/comments`)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an("array");
+          expect(res.body.length).to.equal(2);
+          done();
+        });
+    });
+  });
+  describe("POST /api/articles/:article_id/comments", () => {
+    it("should post a comment to a particular article", done => {
+      request(server)
+        .post(`/api/articles/${usefulData.articles[0]._id}/comments`)
+        .send({ comment: "HELLO, IS IT ME YOU ARE LOOKING FOOOOR" })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.comment.body).to.equal('HELLO, IS IT ME YOU ARE LOOKING FOOOOR');
+          done();
+        });
     });
   });
 });
